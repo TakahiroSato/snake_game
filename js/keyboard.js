fd0a25bf07c62dadab6660b2dc70fbf2a173a179
TakahiroSato <takahiro.sato@aijus.com>
2019-10-11 13:33:21 +0900

    方向キーでのスクロール抑止

--- js/keyboard.js
+++ js/keyboard.js
@@ -9,18 +9,22 @@ let keyboard = {
 
 document.onkeydown = e => {
   let f = {};
+  const prevent = func => {
+    func();
+    e.preventDefault();
+  }
   // p
   f["80"] = () => keyboard.p();
   // escape
   f["27"] = () => keyboard.esc();
   // left
-  f["37"] = () => keyboard.left();
+  f["37"] = () => prevent(keyboard.left);
   // up
-  f["38"] = () => keyboard.up();
+  f["38"] = () => prevent(keyboard.up);
   // right
-  f["39"] = () => keyboard.right();
+  f["39"] = () => prevent(keyboard.right);
   // down
-  f["40"] = () => keyboard.down();
+  f["40"] = () => prevent(keyboard.down);
   if (f[e.keyCode]) f[e.keyCode]();
 };
 
